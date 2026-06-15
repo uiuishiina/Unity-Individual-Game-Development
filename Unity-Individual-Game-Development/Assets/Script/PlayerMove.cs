f14a88c2234450ddca045bfc3f2b21e3a54f7b65
uiuishiina <shiina04jbs10@gmail.com>
2026-06-15 13:18:53 +0900

    aa

---
+++ Unity-Individual-Game-Development/Assets/Script/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    PlayerInput inputsystem;
+    Rigidbody rb;
+    [Header("プレイヤー設定")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float gravity = 9.8f;
+
+    private void Awake() {
+        inputsystem = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+
+        var move = inputsystem.actions["Move"].ReadValue<Vector2>();
+        rb.linearVelocity = new Vector3(move.x * moveSpeed, -gravity * Time.deltaTime, move.y * moveSpeed);
+    }
+}
